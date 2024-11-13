b3b89398f8a84be0e7f939b4b88d869c47b24abf
Sumedh Atreya <127716273+Parzigit@users.noreply.github.com>
2024-11-13 23:25:59 +0530

    Update Contact controller

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,22 +1,17 @@
-const asyncHandler = require("express-async-handler");
-const User = require("../models/userModel");
-const sendEmail = require("../utils/sendEmail");
-
+const asyncHandler =   require("express-async-handler");
+const User =   require("../models/userModel");
+const sendEmail =   require("../utils/sendEmail");
 const contactUs = asyncHandler(async (req, res) => {
   const { subject, message } = req.body;
   const user = await User.findById(req.user._id);
-
   if (!user) {
     res.status(400);
     throw new Error("User not found, please signup");
   }
-
-  //   Validation
   if (!subject || !message) {
     res.status(400);
     throw new Error("Please add subject and message");
   }
-
   const send_to = process.env.EMAIL_USER;
   const sent_from = process.env.EMAIL_USER;
   const reply_to = user.email;
@@ -28,7 +23,6 @@ const contactUs = asyncHandler(async (req, res) => {
     throw new Error("Email not sent, please try again");
   }
 });
-
-module.exports = {
+module.exports={
   contactUs,
 };
