e0295d9616c731024289786e1e29a767352d3b12
Sumedh Atreya <127716273+Parzigit@users.noreply.github.com>
2025-05-31 22:15:03 +0530

    Update Loader.js

--- frontend/src/components/loader/Loader.js
+++ frontend/src/components/loader/Loader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import loaderImg from "../../assets/loader.gif";
+import loaderImg from "../../assets/giphy.gif";
 import ReactDOM from "react-dom";
 import "./Loader.scss";
 
@@ -17,7 +17,7 @@ const Loader = () => {
 export const SpinnerImg = () => {
   return (
     <div className="--center-all">
-      <img src={loaderImg} alt="Loading..." />
+      <img src={loaderImg} alt="Hold on.." />
     </div>
   );
 };
