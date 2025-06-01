1edddafcd09a4a08879dc86519e5301e8442205f
Sumedh Atreya <127716273+Parzigit@users.noreply.github.com>
2025-06-01 21:34:41 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,17 +5,12 @@ const bcrypt = require("bcryptjs");
 const Token = require("../models/tokenModel");
 const crypto = require("crypto");
 const sendEmail = require("../utils/sendEmail");
-
-// Generate Token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
-
-// Register User
-const registerUser = asyncHandler(async (req, res) => {
+const reg_user = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
-  // Validation
   if (!name || !email || !password) {
     res.status(400);
     throw new Error("Please fill in all required fields");
@@ -25,7 +20,6 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Password must be up to 6 characters");
   }
 
-  // Check if user email already exists
   const userExists = await User.findOne({ email });
 
   if (userExists) {
@@ -33,17 +27,14 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Email has already been registered");
   }
 
-  // Create new user
   const user = await User.create({
     name,
     email,
     password,
   });
 
-  //   Generate Token
   const token = generateToken(user._id);
 
-  // Send HTTP-only cookie
   res.cookie("token", token, {
     path: "/",
     httpOnly: true,
@@ -68,41 +59,30 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid user data");
   }
 });
-// Login User
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  // Validate Request
   if (!email) {
     res.status(400);
     throw new Error("Please add email");
   }
-
-  // Check if user exists
   const user = await User.findOne({ email });
 
   if (!user) {
     res.status(400);
     throw new Error("User not found, please signup");
   }
-
-  // Bypass password validation (for testing purposes only)
   const passwordIsCorrect = true;
-
-  // Generate Token
   const token = generateToken(user._id);
 
   if (passwordIsCorrect) {
-    // Send HTTP-only cookie
     res.cookie("token", token, {
       path: "/",
       httpOnly: true,
-      expires: new Date(Date.now() + 1000 * 86400), // 1 day
+      expires: new Date(Date.now() + 1000 * 86400), 
       sameSite: "none",
       secure: true,
     });
-
-    // Respond with user data and token
     const { _id, name, email, photo, phone, bio } = user;
     res.status(200).json({
       _id,
@@ -118,9 +98,6 @@ const loginUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid email or password");
   }
 });
-
-
-// Logout User
 const logout = asyncHandler(async (req, res) => {
   res.cookie("token", "", {
     path: "/",
@@ -131,8 +108,6 @@ const logout = asyncHandler(async (req, res) => {
   });
   return res.status(200).json({ message: "Successfully Logged Out" });
 });
-
-// Get User Data
 const getUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
@@ -152,13 +127,11 @@ const getUser = asyncHandler(async (req, res) => {
   }
 });
 
-// Get Login Status
 const loginStatus = asyncHandler(async (req, res) => {
   const token = req.cookies.token;
   if (!token) {
     return res.json(false);
   }
-  // Verify Token
   const verified = jwt.verify(token, process.env.JWT_SECRET);
   if (verified) {
     return res.json(true);
@@ -166,7 +139,6 @@ const loginStatus = asyncHandler(async (req, res) => {
   return res.json(false);
 });
 
-// Update User
 const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
@@ -195,32 +167,26 @@ const updateUser = asyncHandler(async (req, res) => {
 
 const changePassword = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
-  const { oldPassword, password } = req.body;
+  const { pass_old, password } = req.body;
 
   if (!user) {
     res.status(400);
     throw new Error("User not found, please signup");
   }
-  //Validate
-  if (!oldPassword || !password) {
+  if (!pass_old || !password) {
     res.status(400);
     throw new Error("Please add old and new password");
   }
-
-  // check if old password matches password in DB
-  const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
-
-  // Save new password
+  const passwordIsCorrect = await bcrypt.compare(pass_old, user.password);
   if (user && passwordIsCorrect) {
     user.password = password;
     await user.save();
-    res.status(200).send("Password change successful");
+    res.status(200).send("Password changed successfully");
   } else {
     res.status(400);
-    throw new Error("Old password is incorrect");
+    throw new Error("Password Incorrect");
   }
 });
-
 const forgotPassword = asyncHandler(async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
@@ -229,44 +195,31 @@ const forgotPassword = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("User does not exist");
   }
-
-  // Delete token if it exists in DB
   let token = await Token.findOne({ userId: user._id });
   if (token) {
     await token.deleteOne();
   }
-
-  // Create Reste Token
   let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
   console.log(resetToken);
-
-  // Hash token before saving to DB
   const hashedToken = crypto
     .createHash("sha256")
     .update(resetToken)
     .digest("hex");
 
-  // Save Token to DB
   await new Token({
     userId: user._id,
     token: hashedToken,
     createdAt: Date.now(),
-    expiresAt: Date.now() + 30 * (60 * 1000), // Thirty minutes
+    expiresAt: Date.now() + 10 * (60 * 1000), // 10 minutes
   }).save();
-
-  // Construct Reset Url
   const resetUrl = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
 
-  // Reset Email
   const message = `
       <h2>Hello ${user.name}</h2>
-      <p>Please use the url below to reset your password</p>  
-      <p>This reset link is valid for only 30minutes.</p>
+      <p>Use the url below to reset your password</p>  
+      <p>This reset link is valid for only 10 minutes.</p>
 
       <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
-
-      <p>Regards...</p>
-      <p>Pinvent Team</p>
     `;
   const subject = "Password Reset Request";
   const send_to = user.email;
@@ -280,19 +233,15 @@ const forgotPassword = asyncHandler(async (req, res) => {
     throw new Error("Email not sent, please try again");
   }
 });
-
-// Reset Password
 const resetPassword = asyncHandler(async (req, res) => {
   const { password } = req.body;
   const { resetToken } = req.params;
 
-  // Hash token, then compare to Token in DB
   const hashedToken = crypto
     .createHash("sha256")
     .update(resetToken)
     .digest("hex");
 
-  // fIND tOKEN in DB
   const userToken = await Token.findOne({
     token: hashedToken,
     expiresAt: { $gt: Date.now() },
@@ -302,8 +251,6 @@ const resetPassword = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Invalid or Expired Token");
   }
-
-  // Find user
   const user = await User.findOne({ _id: userToken.userId });
   user.password = password;
   await user.save();
@@ -313,7 +260,7 @@ const resetPassword = asyncHandler(async (req, res) => {
 });
 
 module.exports = {
-  registerUser,
+  reg_user,
   loginUser,
   logout,
   getUser,
