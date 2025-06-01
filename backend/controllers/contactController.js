832c1f5dbf0b4d0789954b15da6f9783bf2fdcc5
Sumedh Atreya <127716273+Parzigit@users.noreply.github.com>
2025-06-01 21:31:45 +0530

    Update contactController.js

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,28 +1,29 @@
-const asyncHandler =   require("express-async-handler");
-const User =   require("../models/userModel");
-const sendEmail =   require("../utils/sendEmail");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const sendEmail = require("../utils/sendEmail");
+
 const contactUs = asyncHandler(async (req, res) => {
   const { subject, message } = req.body;
   const user = await User.findById(req.user._id);
   if (!user) {
     res.status(400);
-    throw new Error("User not found, please signup");
+    throw new Error("Please SignUp");
   }
-  if (!subject || !message) {
+  if(!subject || !message) {
     res.status(400);
-    throw new Error("Please add subject and message");
+    throw new Error("Subject/Message not provided");
   }
-  const send_to = process.env.EMAIL_USER;
-  const sent_from = process.env.EMAIL_USER;
-  const reply_to = user.email;
+  const to=process.env.EMAIL_USER;
+  const from=process.env.EMAIL_USER;
+  const rply=user.email;
   try {
-    await sendEmail(subject, message, send_to, sent_from, reply_to);
-    res.status(200).json({ success: true, message: "Email Sent" });
+    await sendEmail(subject, message, to, from, rply);
+    res.status(200).json({success: true, message: "Sent Successfully"});
   } catch (error) {
     res.status(500);
-    throw new Error("Email not sent, please try again");
+    throw new Error("Please try again");
   }
 });
-module.exports={
+module.exports = {
   contactUs,
 };
