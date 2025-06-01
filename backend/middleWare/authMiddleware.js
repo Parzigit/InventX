ea347c4dd956dd5af6d37ccf1b4315ff66dbabaf
Sumedh Atreya <127716273+Parzigit@users.noreply.github.com>
2025-06-01 21:35:00 +0530

    Update authMiddleware.js

--- backend/middleWare/authMiddleware.js
+++ backend/middleWare/authMiddleware.js
@@ -9,10 +9,7 @@ const protect = asyncHandler(async (req, res, next) => {
       res.status(401);
       throw new Error("Not authorized, please login");
     }
-
-    // Verify Token
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-    // Get user id from token
     const user = await User.findById(verified.id).select("-password");
 
     if (!user) {
