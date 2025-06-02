0fe493316d6dc1e7840e3a6fffd6fe184ff1ef79
Sumedh Atreya <127716273+Parzigit@users.noreply.github.com>
2025-06-02 22:51:09 +0530

    Update Login.js

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,57 +1,49 @@
 import React, { useState } from "react";
 import styles from "./auth.module.scss";
-import { TiUserAddOutline } from "react-icons/ti";
+import { BiLogIn } from "react-icons/bi";
 import Card from "../../components/card/Card";
-import { toast } from "react-toastify";
-import { registerUser, validateEmail } from "../../services/authService";
-import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { loginUser, validateEmail } from "../../services/authService";
 import { SET_LOGIN, SET_NAME } from "../../redux/features/auth/authSlice";
 import Loader from "../../components/loader/Loader";
 
 const initialState = {
-  name: "",
-  email: "",
+  email: "@gmail.com",
   password: "",
-  password2: "",
 };
 
-const Register = () => {
+const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const [formData, setformData] = useState(initialState);
-  const { name, email, password, password2 } = formData;
+  const { email, password } = formData;
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setformData({ ...formData, [name]: value });
   };
 
-  const register = async (e) => {
+  const login = async (e) => {
     e.preventDefault();
 
-    if (!name || !email || !password) {
+    if (!email || !password) {
       return toast.error("All fields are required");
     }
-    if (password.length < 6) {
-      return toast.error("Passwords must be up to 6 characters");
-    }
+
     if (!validateEmail(email)) {
       return toast.error("Please enter a valid email");
     }
-    if (password !== password2) {
-      return toast.error("Passwords do not match");
-    }
 
     const userData = {
-      name,
       email,
       password,
     };
     setIsLoading(true);
     try {
-      const data = await registerUser(userData);
+      const data = await loginUser(userData);
       console.log(data);
       await dispatch(SET_LOGIN(true));
       await dispatch(SET_NAME(data.name));
@@ -68,19 +60,11 @@ const Register = () => {
       <Card>
         <div className={styles.form}>
           <div className="--flex-center">
-            <TiUserAddOutline size={35} color="#999" />
+            <BiLogIn size={35} color="#999" />
           </div>
-          <h2>Register</h2>
+          <h2>Login</h2>
 
-          <form onSubmit={register}>
-            <input
-              type="text"
-              placeholder="Name"
-              required
-              name="name"
-              value={name}
-              onChange={handleInputChange}
-            />
+          <form onSubmit={login}>
             <input
               type="email"
               placeholder="Email"
@@ -97,23 +81,17 @@ const Register = () => {
               value={password}
               onChange={handleInputChange}
             />
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              required
-              name="password2"
-              value={password2}
-              onChange={handleInputChange}
-            />
             <button type="submit" className="--btn --btn-primary --btn-block">
-              Register
+              Login
             </button>
           </form>
+          <br></br>
+          <Link to="/forgot"><div style={{color:'red'}}>Forgot Password?</div></Link>
 
           <span className={styles.register}>
-            <Link to="/">Home</Link>
-            <p> &nbsp; Already have an account? &nbsp;</p>
-            <Link to="/login"><div style={{color:'red'}}>Login</div></Link>
+          <Link to="/">Home</Link>
+            <p> &nbsp; New here? &nbsp;</p>
+            <Link to="/register" ><div style={{color:'blue'}}>Register</div></Link>
           </span>
         </div>
       </Card>
@@ -121,4 +99,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
