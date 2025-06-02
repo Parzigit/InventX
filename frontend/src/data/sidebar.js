59ad77dc075aaebce086c96ab61585fdda870fa3
Sumedh Atreya <127716273+Parzigit@users.noreply.github.com>
2025-06-02 22:40:17 +0530

    Update sidebar.js

--- frontend/src/data/sidebar.js
+++ frontend/src/data/sidebar.js
@@ -1,4 +1,4 @@
-import { FaTh, FaRegChartBar, FaCommentAlt } from "react-icons/fa";
+import { FaTh, FaRegChartBar, FaCommentAlt, FaUser, FaUserEdit  } from "react-icons/fa";
 import { BiImageAdd } from "react-icons/bi";
 
 const menu = [
@@ -13,15 +13,17 @@ const menu = [
     path: "/add-product",
   },
   {
-    title: "Account",
+    title: "View Account",
     icon: <FaRegChartBar />,
     childrens: [
       {
         title: "Profile",
+        icon: <FaUser />,
         path: "/profile",
       },
       {
         title: "Edit Profile",
+        icon: <FaUserEdit />,
         path: "/edit-profile",
       },
     ],
