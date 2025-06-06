ea604a13d5f0e728dcc7eb968944b973a2cefa68
Sumedh Atreya <127716273+Parzigit@users.noreply.github.com>
2025-06-06 20:11:47 +0530

    Update Sidebar.js

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -1,51 +1,52 @@
 "use client"
 import { useState } from "react"
 import "./Sidebar.scss"
-import { HiMenuAlt3 } from "react-icons/hi"
 import logo from "../../assets/log.png"
 import menu from "../../data/sidebar"
 import SidebarItem from "./SidebarItem"
-import { useNavigate } from "react-router-dom"
 
 const Sidebar = ({ children }) => {
   const [isOpen, setIsOpen] = useState(true)
   const toggle = () => setIsOpen(!isOpen)
-  // const navigate = useNavigate()
-
-  // const goHome = () => {
-  //   navigate("/")
-  // }
 
   return (
     <div className="layout">
-      <div className="sidebar" style={{ width: isOpen ? "230px" : "70px" }}>
+      <div className="sidebar" style={{ width: isOpen ? "230px" : "60px" }}>
         <div className="top_section">
-          <div className="logo" style={{ display: isOpen ? "block" : "inline" }&&{marginLeft:isOpen?"60px":"-5px"}}>
-          {/* <HiMenuAlt3 onClick=/> */}
-          <img src={logo || "/placeholder.svg"} alt="logo" 
-              paddingtop={1}
-              size={35}
-              style={{ cursor: "pointer" }}
-              onClick={toggle} 
+          <div
+            className="logo"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: isOpen ? "10px 0" : "10px 0",
+            }}
+          >
+            <img
+              src={logo || "/placeholder.svg"}
+              alt="logo"
+              style={{
+                cursor: "pointer",
+                width: "35px",
+                height: "35px",
+                objectFit: "contain",
+              }}
+              onClick={toggle}
             />
           </div>
-          {/* 
-          <div
-            className="bars"
-            style={{ marginLeft: isOpen ? "120px" : "120px" }}
-          >
-            
-          </div> */}
         </div>
-        {menu.map((item, index) => {
-          return <SidebarItem key={index} item={item} isOpen={isOpen} />;
-        })}
+        <div className="menu-items">
+          {menu.map((item, index) => {
+            return <SidebarItem key={index} item={item} isOpen={isOpen} />
+          })}
+        </div>
       </div>
 
       <main
         style={{
-          paddingLeft: isOpen ? "650px" : "250px",
-          transition: "all .8s",
+          paddingLeft: isOpen ? "230px" : "60px",
+          transition: "all .3s",
+          width: "100%",
         }}
       >
         {children}
@@ -55,5 +56,3 @@ const Sidebar = ({ children }) => {
 }
 
 export default Sidebar
-
-
