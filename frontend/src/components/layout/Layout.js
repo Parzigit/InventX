578b59ef350459dc6da0577650eca75b2c4ee03d
Sumedh Atreya <127716273+Parzigit@users.noreply.github.com>
2025-06-06 20:07:51 +0530

    Update Layout.js

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Footer from "../footer/Footer";
+// import Footer from "../footer/Footer";
 import Header from "../header/Header";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <div style={{ minHeight: "80vh" }} className="--pad">
+      <div style={{ minHeight: "20vh" ,minWidth:"75vh"}} className="--pad">
         {children}
       </div>
-      <Footer />
+      // <Footer />
     </>
   );
 };
